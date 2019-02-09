c0bd3cd957bafaf4f228963d699262158796cb75
Suwidnyana <grayfullbuster522@gmail.com>
2019-02-09 01:42:03 -0500

    Tugas Pak Gustra Tercinta

---
+++ app/src/main/java/com/example/uas_1615323053/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.uas_1615323053;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //layout
+
+
+
+    }
+}
